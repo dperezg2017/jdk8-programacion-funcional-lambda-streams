c774198e03aa075fd9c6eaa4c016e2b06611cbad
dperezg2017 <dperezg2017@gmail.com>
2020-03-05 01:38:58 -0500

    zip

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,11 @@
 
 m
 =utilitario-java-11/src/test/java/com/util/BiConsumerTest.java,3/c/3c3f0bb6afc665241d45e34abde8eca7345278cc
-i
-9utilitario-java-11/src/test/java/com/util/Utilitario.java,3/4/34577ebaa461b56722e96124eb2d810a95e659ec
+m
+=utilitario-java-11/src/main/java/com/util/entity/Persona.java,4/9/49aee74b1aaa2b94c064be89813d1a33c4e88d89
+t
+Dutilitario-java-11/src/test/java/com/util/UtilitarioCollections.java,7/4/7455064a67ea88a537e8fc7e61535137f7a7d6db
+s
+Cutilitario-java-11/src/test/java/com/util/UtilitarioCollectors.java,0/d/0d739827c88682fb40d2e665ad49a19bc56a8cd4
+l
+<utilitario-java-11/src/test/java/com/util/UtilitarioZip.java,6/d/6d685306b8bf26ddbfbb54de501d6c6a13bebe3c
--- utilitario-java-11/src/test/java/com/util/UtilitarioZip.java
+++ utilitario-java-11/src/test/java/com/util/UtilitarioZip.java
@@ -1,56 +1,131 @@
 package com.util;
-
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.zip.Deflater;
-import java.util.zip.Inflater;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.util.zip.*;
 
 public class UtilitarioZip {
 
     private static final Logger LOOGER = LoggerFactory.getLogger(UtilitarioZip.class);
 
     @Test
-    public void utilitario_comprimir_descomprimir_zip() {
-        try {
-            // archivo que extraera
-            String zipFilePath = "/Users/deyvisperez/deyvizperez/udemy/jdk8-programacion-funcional-lambda-streams/utilitario-java-11/xmlTest4.xml";
-            FileOutputStream fos = new FileOutputStream(zipFilePath);
-            ZipOutputStream zos = new ZipOutputStream(fos);
+    public void comprimir_directorio_zip() throws IOException {
+        String sourceFile = "/Users/deyvisperez/deyvizperez/udemy/jdk8-programacion-funcional-lambda-streams/utilitario-java-11/";
+        FileOutputStream fos = new FileOutputStream("directorioComprimido.zip");
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        File fileToZip = new File(sourceFile);
 
-            // ruta a descomprimir
-            File rootFolder = new File("/Users/deyvisperez/deyvizperez/udemy/jdk8-programacion-funcional-lambda-streams/utilitario-java-11/comprimir");
-            for (File file : rootFolder.listFiles()) {
-                writeToZip(file, zos);
-                System.out.println("Zipping: " + file.getName());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        zipFile(fileToZip, fileToZip.getName(), zipOut);
+        zipOut.close();
+        fos.close();
     }
 
-    private static void writeToZip(File file, ZipOutputStream zos) throws IOException {
-        FileInputStream fis = new FileInputStream(file);
-        ZipEntry zipEntry = new ZipEntry(file.getAbsolutePath());
-        zos.putNextEntry(zipEntry);
-
+    @Test
+    public void comprimir_file() throws IOException {
+        String sourceFile = "/Users/deyvisperez/deyvizperez/udemy/jdk8-programacion-funcional-lambda-streams/utilitario-java-11/xmlTest2.xml";
+        FileOutputStream fos = new FileOutputStream("FileComprimido.zip");
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        File fileToZip = new File(sourceFile);
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+        zipOut.putNextEntry(zipEntry);
         byte[] bytes = new byte[1024];
         int length;
-        while ((length = fis.read(bytes)) >= 0)
-            zos.write(bytes, 0, length);
+        while((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+        zipOut.close();
+        fis.close();
+        fos.close();
+    }
 
-        zos.closeEntry();
+    @Test
+    public void comprimir_files() throws IOException {
+        List<String> srcFiles = Arrays.asList(
+                "/Users/deyvisperez/deyvizperez/udemy/jdk8-programacion-funcional-lambda-streams/utilitario-java-11/fileTest1.xml",
+                "/Users/deyvisperez/deyvizperez/udemy/jdk8-programacion-funcional-lambda-streams/utilitario-java-11/fileTest2.xml");
+        FileOutputStream fos = new FileOutputStream("filesComprimidos.zip");
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        for (String srcFile : srcFiles) {
+            File fileToZip = new File(srcFile);
+            FileInputStream fis = new FileInputStream(fileToZip);
+            ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+            zipOut.putNextEntry(zipEntry);
+
+            byte[] bytes = new byte[1024];
+            int length;
+            while((length = fis.read(bytes)) >= 0) {
+                zipOut.write(bytes, 0, length);
+            }
+            fis.close();
+        }
+        zipOut.close();
+        fos.close();
+    }
+
+    @Test
+    public void descomprimir_zip() throws IOException {
+        String fileZip = "/Users/deyvisperez/deyvizperez/udemy/jdk8-programacion-funcional-lambda-streams/utilitario-java-11/filesComprimidos.zip";
+        File destDir = new File("/Users/deyvisperez/deyvizperez/udemy/jdk8-programacion-funcional-lambda-streams/utilitario-java-11/descomprimir/");
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = newFile(destDir, zipEntry);
+            FileOutputStream fos = new FileOutputStream(newFile);
+            int len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+            fos.close();
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+    }
+
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+    private void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
+        if (fileToZip.isHidden()) {
+            return;
+        }
+        if (fileToZip.isDirectory()) {
+            if (fileName.endsWith("/")) {
+                zipOut.putNextEntry(new ZipEntry(fileName));
+                zipOut.closeEntry();
+            } else {
+                zipOut.putNextEntry(new ZipEntry(fileName + "/"));
+                zipOut.closeEntry();
+            }
+            File[] children = fileToZip.listFiles();
+            for (File childFile : children) {
+                zipFile(childFile, fileName + "/" + childFile.getName(), zipOut);
+            }
+            return;
+        }
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileName);
+        zipOut.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
         fis.close();
     }
 
